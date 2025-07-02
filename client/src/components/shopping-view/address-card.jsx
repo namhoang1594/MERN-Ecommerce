3ee1af50ed98063f3d30d6f73afa8c10688619f1
namhoang1594 <hnn.nam01@gmail.com>
2025-07-02 17:18:47 +0700

    crud address form

---
+++ client/src/components/shopping-view/address-card.jsx
@@ -0,0 +1,23 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Label } from "../ui/label";
+
+function AddressCard({ addressInfo, handleEditAddress, handleDeleteAddress }) {
+  return (
+    <Card>
+      <CardContent className="grid gap-4 p-4">
+        <Label>Address: {addressInfo?.address}</Label>
+        <Label>City: {addressInfo?.city}</Label>
+        <Label>Phone: {addressInfo?.phone}</Label>
+        <Label>Pincode: {addressInfo?.pincode}</Label>
+        <Label>Notes: {addressInfo?.notes}</Label>
+      </CardContent>
+      <CardFooter className="flex justify-between p-2">
+        <Button onClick={() => handleEditAddress(addressInfo)}>Edit</Button>
+        <Button onClick={() => handleDeleteAddress(addressInfo)}>Delete</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default AddressCard;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const shopProductRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
 
 // create a database connection
 mongoose
@@ -39,5 +40,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductRouter);
 app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
